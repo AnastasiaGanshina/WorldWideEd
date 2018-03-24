7af958a70b3ef3154e63b94d0f8f24a804bb673e
AnastasiaGanshina <36440279+AnastasiaGanshina@users.noreply.github.com>
2018-03-24 22:39:24 +0300

    release v1

---
+++ Documents/GitHub/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+
+	
+<html>
+
+<head>
+<link rel = "stylesheet" href = "CSSstyle.css">
+<meta charset="utf-8">
+</head>
+
+<?php include "header.php" ?>
+<?php include "left.php" ?>
+
+<div class="Main">
+
+<p><h1>КАКАЯ-НИБУДЬ МОТИВИРУЮЩАЯ КАРТИНКА</h1></p>
+
+
+<p>We would like to welcome everyone to the Worldwide Education Program. This is a project made by students who wanted to facilitate the research process. Here you can discover your abilities for future education and realize that you have an opportunity for achieving your goals. In other words, we are capable to help you with one of the most difficult problem - finding a university. Make sure that you understand that it is only your responsibility, and we just make your search easier for our users. Here we will introduce you to any opportunity you have with your individual character. However, for a successful search we advise you to understand some important features of yourself. </p>
+<p>КАРТИНКА№3</p>
+<p>First of all, you better ask yourself questions like "In what field am I interested in?", ‘Do I want to do it during my whole life?" or "Do I have any abilities for that?". In other words, think about your personal interests and capacity. We emphasize it because not many people are happy with their choice and they regret about it. For this reason, it is the most important point for success. However, do not be afraid of it if you do not know exactly what are you interested in․ For a better result you should just spend more time thinking.</p>
+<p>КАРТИНКА№4</p>
+<p>The next point is that as soon as you find the requirements, you can start working yourself. For instance, if you know rules for the year you have more chances to be accepted in the university (in case you improve) in comparison to a someone who does not know the demands at all. And the best thing is that our site can help you out with it. Now you should just chose your preferences and click the bottom note "read more".</p>
+<p> For more comfortable service read the instruction below.</p>
+<p>Login or sign to our site.</p> 
+<p>Click the bottom "FIND a UNIVERSITY" on the left.</p> 
+<p>3. Choose personal preferences in the menu.</p> 
+<p>4. If you want to read more click the bottom sign “READ MORE" in the information about a specific university.</p>
+</div>
+
+<?php include_once "footer.php"?>
+</html>
